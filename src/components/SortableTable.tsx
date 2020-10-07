c57894bbe0a28e7a6b668d2b87aad2a2748b9cae
Marek Choiński <marekchoinski17@gmail.com>
2020-10-07 20:59:17 +0200

    Implementing sorting

--- src/components/SortableTable.tsx
+++ src/components/SortableTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { ReactComponent as ArrowOpen } from '../assets/arrowOpen.svg';
 import { ReactComponent as ArrowClose } from '../assets/arrowClose.svg';
 import { Planet, planetLabels } from "../interfaces/Planet.interface";
@@ -7,29 +7,54 @@ type Props = {
     data: Planet[],
 };
 
+const sortByKey = (array: any[], key: string) => {
+    return array.sort((a, b) => {
+        const x = a[key];
+        const y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+}
+
 const SortableTable: React.FC<Props> = props => {
 
     const {
         data
     } = props;
 
-    const keys: React.ReactNode = Object.keys(data[0]).map((k) => {
-        return (<th key={k}>
-            <span className="sort-by">
-                {(planetLabels as any)[k]}
-            </span>
-        </th>)
-    });
+    const [ascending, setAscending] = useState(true);
+    const [sortMethod, setSortMethod] = useState("name");
+    const [sortedData, setSortedData] = useState<Planet[]>(data);
+
+    useEffect(() => {
+        //NOTE: sorting is inplace, so need to make new array
+        const newSorted = [...sortedData];
+        setSortedData(sortByKey(newSorted, sortMethod));
+        console.log(sortedData);
+
+    }, [ascending, sortMethod]);
 
     return (
         <table className="sortableTable">
             <tr>
-                {keys}
+                {
+                    Object.keys(sortedData[0]).map((k) => {
+                        return (<th
+                            key={k}
+                            onClick={() => { setSortMethod(k) }}
+                        >
+                            <span className="sort-by">
+                                {(planetLabels as any)[k]}
+                            </span>
+                        </th>)
+                    })
+                }
             </tr>
             {
-                data.map(planet => {
+                sortedData.map(planet => {
+                    console.log(planet.name);
+
                     return (
-                        <tr>
+                        <tr key={planet.name}>
                             {Object.values(planet).map((v) => {
                                 // NOTE: climate is array, but in mockup it is value
                                 if (Array.isArray(v)) {
