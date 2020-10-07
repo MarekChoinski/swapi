2dff3ae2172d2cb9295e3486b79c504a32224ecc
Marek Choiński <marekchoinski17@gmail.com>
2020-10-07 15:32:45 +0200

    Create Collapse component

---
+++ src/components/Collapse.tsx
@@ -0,0 +1,29 @@
+import React, { ReactElement } from "react"
+import ArrowOpen from '../assets/arrowOpen.svg';
+
+type Props = {
+    children: React.ReactNode,
+    title: string,
+};
+
+const Collapse: React.FC<Props> = props => {
+
+    const {
+        children,
+        title
+    } = props;
+
+    return (
+        <section className="collapse">
+            <header>
+                <span className="collapse__title">
+                    {title}
+                </span>
+                <ArrowOpen />
+            </header>
+        </section>
+
+    );
+}
+
+export default Collapse;
