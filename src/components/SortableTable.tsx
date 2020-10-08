76d0d2324cb3e562127288b911d8d73d2f718684
Marek Choiński <marekchoinski17@gmail.com>
2020-10-08 23:42:36 +0200

    Fix tbody

--- src/components/SortableTable.tsx
+++ src/components/SortableTable.tsx
@@ -69,49 +69,53 @@ const SortableTable: React.FC<Props> = props => {
             {!loading && sortedData.length ?
 
                 <table className="sortableTable">
-                    <tr>
-                        {
-                            Object.keys(sortedData[0]).map((k) => {
-                                if (k !== '__typename')
-                                    return (<th
-                                        key={k}
-                                        onClick={() => { setSortingMethodWithAscending(k) }}
-                                    >
-                                        <span className={`
+                    <tbody>
+
+
+                        <tr>
+                            {
+                                Object.keys(sortedData[0]).map((k) => {
+                                    if (k !== '__typename')
+                                        return (<th
+                                            key={k}
+                                            onClick={() => { setSortingMethodWithAscending(k) }}
+                                        >
+                                            <span className={`
                                             sortBy 
                                             ${k === sortMethod ? "actualMethod" : ""}
                                             ${(k === sortMethod && ascending) ? "ascending" : ""}
                                             ${(k === sortMethod && !ascending) ? "descending" : ""}
                                             `}
-                                        >
-                                            {(planetLabels as any)[k]}
-                                        </span>
-                                    </th>)
-                            })
-                        }
-                    </tr>
-                    {
-                        sortedData.map(planet => {
-                            return (
-                                <tr key={planet.name}>
-                                    {Object.entries(planet).map(([k, v], _) => {
-                                        // NOTE: climate is array, but in mockup it is value
-                                        if (Array.isArray(v)) {
-                                            v = v[0];
-                                        }
-                                        if (k !== '__typename') {
-                                            if (v === null) {
-                                                return (<td key={v}>unknown</td>);
+                                            >
+                                                {(planetLabels as any)[k]}
+                                            </span>
+                                        </th>)
+                                })
+                            }
+                        </tr>
+                        {
+                            sortedData.map(planet => {
+                                return (
+                                    <tr key={planet.name}>
+                                        {Object.entries(planet).map(([k, v], _) => {
+                                            // NOTE: climate is array, but in mockup it is value
+                                            if (Array.isArray(v)) {
+                                                v = v[0];
+                                            }
+                                            if (k !== '__typename') {
+                                                if (v === null) {
+                                                    return (<td key={v}>unknown</td>);
+                                                }
+
+                                                return (<td key={v}>{v}</td>);
                                             }
 
-                                            return (<td key={v}>{v}</td>);
-                                        }
-
-                                    })}
-                                </tr>
-                            );
-                        })
-                    }
+                                        })}
+                                    </tr>
+                                );
+                            })
+                        }
+                    </tbody>
                 </table> :
                 <div>LOADING</div>
             }
