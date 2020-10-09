ef5fdbe724766f9ea7592ff13f2a5c9630691409
Marek Choiński <marekchoinski17@gmail.com>
2020-10-09 16:17:31 +0200

    Updates

---
+++ precache-manifest.97e645a2154215eb14be86455cf9fc70.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d13b0b3ea1fda23aefd7aa16c0b6ed1a",
+    "url": "/swapi/index.html"
+  },
+  {
+    "revision": "97bc5cfc681e81019ec7",
+    "url": "/swapi/static/css/main.82bd15af.chunk.css"
+  },
+  {
+    "revision": "7c9a3729db5e1e55a822",
+    "url": "/swapi/static/js/2.22f9d995.chunk.js"
+  },
+  {
+    "revision": "348dea1cfac3793e3b2f0c9fa62173f0",
+    "url": "/swapi/static/js/2.22f9d995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97bc5cfc681e81019ec7",
+    "url": "/swapi/static/js/main.992c2563.chunk.js"
+  },
+  {
+    "revision": "541955dd686d7b6ec0c0",
+    "url": "/swapi/static/js/runtime-main.aa1e55f8.js"
+  },
+  {
+    "revision": "81e948ad0179ac79be6c28982906dae1",
+    "url": "/swapi/static/media/arrowClose.81e948ad.svg"
+  },
+  {
+    "revision": "da45c4c0de0efd2c6e1fd463b04d3b3f",
+    "url": "/swapi/static/media/arrowOpen.da45c4c0.svg"
+  },
+  {
+    "revision": "4fdbb74fd733a63657da172fdebf0feb",
+    "url": "/swapi/static/media/delete.4fdbb74f.svg"
+  },
+  {
+    "revision": "5495be491f8535b3740b1f2f483d91f9",
+    "url": "/swapi/static/media/loader.5495be49.svg"
+  },
+  {
+    "revision": "79977f053d6f42e5e0575fa2c33b24cd",
+    "url": "/swapi/static/media/logo.79977f05.svg"
+  }
+]);
