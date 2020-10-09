71808f9811fc513d7fe3f8b3bfb3a47de269566f
Marek Choiński <marekchoinski17@gmail.com>
2020-10-09 16:12:13 +0200

    Mobile view of sortable table

--- src/components/SortableTable.tsx
+++ src/components/SortableTable.tsx
@@ -91,55 +91,87 @@ const SortableTable: React.FC<Props> = props => {
     return (
         <Collapse title={title} show={show} onClick={setShowFetchData}>
             {!loading && sortedData.length ?
-
-                <table className="sortableTable">
-                    <tbody>
-
-
-                        <tr>
-                            {
-                                Object.keys(sortedData[0]).map((k) => {
-                                    if (k !== '__typename')
-                                        return (<th
-                                            key={k}
-                                            onClick={() => { setSortingMethodWithAscending(k) }}
-                                        >
-                                            <span className={`
+                <>
+                    <table className="sortableTable">
+                        <tbody>
+                            <tr>
+                                {
+                                    Object.keys(sortedData[0]).map((k) => {
+                                        if (k !== '__typename')
+                                            return (<th
+                                                key={k}
+                                                onClick={() => { setSortingMethodWithAscending(k) }}
+                                            >
+                                                <span className={`
                                             sortBy 
                                             ${k === sortMethod ? "actualMethod" : ""}
                                             ${(k === sortMethod && ascending) ? "ascending" : ""}
                                             ${(k === sortMethod && !ascending) ? "descending" : ""}
                                             `}
-                                            >
-                                                {(planetLabels as any)[k]}
-                                            </span>
-                                        </th>)
+                                                >
+                                                    {(planetLabels as any)[k]}
+                                                </span>
+                                            </th>)
+                                    })
+                                }
+                            </tr>
+                            {
+                                sortedData.map(planet => {
+                                    return (
+                                        <tr key={planet.name}>
+                                            {Object.entries(planet).map(([k, v], _) => {
+                                                // NOTE: climate is array, but in mockup it is value
+                                                if (Array.isArray(v)) {
+                                                    v = v[0];
+                                                }
+                                                if (k !== '__typename') {
+                                                    if (v === null) {
+                                                        return (<td key={planet.name + k}>unknown</td>);
+                                                    }
+                                                    return (<td key={planet.name + k}>{v}</td>);
+                                                }
+
+                                            })}
+                                        </tr>
+                                    );
                                 })
                             }
-                        </tr>
-                        {
-                            sortedData.map(planet => {
-                                return (
-                                    <tr key={planet.name}>
-                                        {Object.entries(planet).map(([k, v], _) => {
-                                            // NOTE: climate is array, but in mockup it is value
-                                            if (Array.isArray(v)) {
-                                                v = v[0];
-                                            }
-                                            if (k !== '__typename') {
-                                                if (v === null) {
-                                                    return (<td key={planet.name + k}>unknown</td>);
-                                                }
-                                                return (<td key={planet.name + k}>{v}</td>);
-                                            }
+                        </tbody>
+                    </table>
+                    <div className="mobileSortableTable">
+                        {sortedData.map(planet => {
+                            return (
+                                <div key={planet.name} className="mobileSortableTable__wrapper">
+                                    {Object.entries(planet).map(([k, v], _) => {
+                                        // NOTE: climate is array, but in mockup it is value
+                                        if (Array.isArray(v)) {
+                                            v = v[0];
+                                        }
+                                        if (k !== '__typename') {
 
-                                        })}
-                                    </tr>
-                                );
-                            })
-                        }
-                    </tbody>
-                </table> :
+                                            if (v === null) {
+                                                return (
+                                                    <div className="mobileSortableTable__row">
+                                                        <div>{(planetLabels as any)[k]}</div>
+                                                        <div key={planet.name + k}>unknown</div>
+                                                    </div>
+                                                );
+                                            }
+                                            return (
+                                                <div className="mobileSortableTable__row">
+                                                    <div>{(planetLabels as any)[k]}</div>
+                                                    <div key={planet.name + k}>{v}</div>
+                                                </div >
+                                            );
+                                        }
+
+                                    })}
+                                </div>
+                            );
+                        })}
+                    </div>
+                </>
+                :
                 <Loader />
             }
         </Collapse>
